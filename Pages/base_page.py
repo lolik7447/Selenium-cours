db179de95c8c90cd96d8cde2613637d8b3c5dd3c
lolichka <123555195+lolik7447@users.noreply.github.com>
2023-06-21 20:39:06 +0700

    Delete Pages directory

--- Pages/base_page.py
+++
@@ -1,9 +0,0 @@
-from .pages.main_page import MainPage
-
-class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
-
-    def open(self): 
-        self.browser.get(self.url)
